35784524a9fd026f2a79e469ea3d3b949179a647
Александр Романов <roman.ulbricht@yandex.ru>
2022-10-05 14:33:47 +0300

    Popitka1

--- homework.py
+++ homework.py
@@ -39,14 +39,17 @@ HOMEWORK_STATUSES = {
 
 
 class NotSendMessageError(Exception):
+    """Классы ошибок"""
     pass
 
 
 class NonStatusCodeError(Exception):
+    """Классы ошибок"""
     pass
 
 
 class WrongStatusCodeError(Exception):
+    """Классы ошибок"""
     pass
 
 
